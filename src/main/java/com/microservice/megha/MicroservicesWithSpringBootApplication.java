460400f1594f5db99a866bd754cdaca9549a37a4
megha <stutimegha2019@gmail.com>
2023-09-25 19:19:10 +0530

    Day 1

---
+++ src/main/java/com/microservice/megha/MicroservicesWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.megha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesWithSpringBootApplication.class, args);
+	}
+
+}
